f5de5ce4cdf0f49994ad75ee3d25d7df2e25eb16
JunH <tjdtls690@gmail.com>
2022-06-22 20:00:14 +0900

    :sparkles: feat : 숫자가 1 ~ 9 안에 속하는지 검증 기능 구현

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 ## 입력
 - [ ] 컴퓨터에게 3개의 랜덤 숫자를 입력받는다.  Computer#createRandomNumber()
   - [x] 3개의 숫자가 모두 달라야 한다.  Judgment#isDifferent()
-  - [ ] 1 ~ 9 까지의 숫자만 들어와야 한다.  Judgment#isInRange()
+  - [x] 1 ~ 9 까지의 숫자만 들어와야 한다.  Judgment#isInRange()
 - [ ] 사용자에게 3개의 숫자를 입력받는다.  Player#inputNumber()
   - [x] 3개의 숫자가 모두 달라야 한다.  Judgment#isDifferent()
-  - [ ] 1 ~ 9 까지의 숫자만 들어와야 한다.  Judgment#isInRange()
+  - [x] 1 ~ 9 까지의 숫자만 들어와야 한다.  Judgment#isInRange()
   
 ## 도메인
 - [ ] 컴퓨터 숫자와 플레이어 숫자를 비교해서 결과값을 도출한다.  Referee#compare()
-  - [ ] 낫싱 : 같은 숫자가 아예 없는 경우  (enum)AssistantReferee#isNothing()
-  - [ ] 스트라이크 : 같은 숫자, 같은 위치  (enum)AssistantReferee#isStrike()
-  - [ ] 볼 : 같은 숫자, 다른 위치  (enum)AssistantReferee#isBall()
+  - [ ] 낫싱 : 같은 숫자가 아예 없는 경우  AssistantReferee#isNothing()
+  - [ ] 스트라이크 : 같은 숫자, 같은 위치  AssistantReferee#isStrike()
+  - [ ] 볼 : 같은 숫자, 다른 위치  AssistantReferee#isBall()
 - [ ] 결과값을 통해 정답 여부를 판단한다.  Referee#isCorrect()
   - [ ] 정답 : 3 strike  
   - [ ] 오답 : not 3 strike
--- src/test/java/com/jun_hyeok/baseball_game/domain/JudgmentTest.java
+++ src/test/java/com/jun_hyeok/baseball_game/domain/JudgmentTest.java
@@ -2,6 +2,7 @@ package com.jun_hyeok.baseball_game.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -24,8 +25,16 @@ public class JudgmentTest {
     @ParameterizedTest
     @DisplayName("해당 숫자가 먼저 저장된 숫자들과 다른지 검증")
     @CsvSource({"1, false", "3, false", "4, true", "9, true"})
-    void isDifferentNumbers(int num, boolean assertBool){
+    void isDifferent(int num, boolean assertBool){
         Judgment judgment = new Judgment();
         assertEquals(judgment.isDifferent(comNumBox, num), assertBool);
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자가 1 ~ 9 안에 속하는지 검증")
+    @CsvSource({"1, true", "9, true", "10, false", "0, false"})
+    void isInRange(int num, boolean assertBool){
+        Judgment judgment = new Judgment();
+        assertEquals(judgment.isInRange(num), assertBool);
     }
 }
