e84f9aba24f0aaa2817b3a841a9561768b715612
JunH <tjdtls690@gmail.com>
2022-06-22 21:29:16 +0900

    :white_check_mark: test : Judgment 테스트 클래스 수정

--- src/test/java/com/jun_hyeok/baseball_game/domain/JudgmentTest.java
+++ src/test/java/com/jun_hyeok/baseball_game/domain/JudgmentTest.java
@@ -1,33 +1,21 @@
 package com.jun_hyeok.baseball_game.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JudgmentTest {
-    private List<Integer> comNumBox;
-
-    @BeforeEach
-    void setUp() {
-        comNumBox = Arrays.asList(1, 2, 3);
-    }
 
     @ParameterizedTest
     @DisplayName("해당 숫자가 먼저 저장된 숫자들과 다른지 검증")
     @CsvSource({"1, false", "3, false", "4, true", "9, true"})
     void isDifferent(int num, boolean assertBool){
         Judgment judgment = new Judgment();
-        assertEquals(judgment.isDifferent(comNumBox, num), assertBool);
+        assertEquals(judgment.isDifferent(Arrays.asList(1, 2, 3), num), assertBool);
     }
 
     @ParameterizedTest
