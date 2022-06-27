8c0151552b5eef0958dbc699b36c965b7f243610
JunH <tjdtls690@gmail.com>
2022-06-27 16:47:14 +0900

    :sparkles: feat : 숫자 비교 후, 결과값 실시간 출력 기능 구현

---
+++ src/main/java/com/jun_hyeok/baseball_game/output/ResultView.java
@@ -0,0 +1,40 @@
+package com.jun_hyeok.baseball_game.output;
+
+import com.jun_hyeok.baseball_game.domain.Referee;
+
+public class ResultView {
+    public void resultPrint(Referee result){
+        String resultMessage = "";
+        resultMessage += getBallMessage(result);
+        resultMessage += getStrikeMessage(result);
+        resultMessage += getNothingMessage(result);
+        System.out.println(resultMessage);
+    }
+
+    private String getNothingMessage(Referee result) {
+        int ball = result.getBall();
+        int strike = result.getStrike();
+        if(ball == 0 && strike == 0){
+            return "Nothing";
+        }
+        return "";
+    }
+
+    private String getStrikeMessage(Referee result) {
+        int strike = result.getStrike();
+        if(strike > 0){
+            return strike + "스트라이크";
+        }
+        return "";
+    }
+
+    private String getBallMessage(Referee result) {
+        int ball = result.getBall();
+        if(ball > 0){
+            return ball + "볼 ";
+        }
+        return "";
+    }
+
+
+}
