9bb17ed4796641e82a7b88ecbc13761421525f12
JunH <tjdtls690@gmail.com>
2022-06-27 16:53:53 +0900

    :sparkles: feat : 정답 시, 축하 및 다시시작 여부 묻기 출력 기능 구현

--- src/main/java/com/jun_hyeok/baseball_game/output/ResultView.java
+++ src/main/java/com/jun_hyeok/baseball_game/output/ResultView.java
@@ -3,6 +3,13 @@ package com.jun_hyeok.baseball_game.output;
 import com.jun_hyeok.baseball_game.domain.Referee;
 
 public class ResultView {
+
+    public static final String ASK_RESTART_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void askRestartPrint(){
+        System.out.println(ASK_RESTART_MESSAGE);
+    }
+
     public void resultPrint(Referee result){
         String resultMessage = "";
         resultMessage += getBallMessage(result);
